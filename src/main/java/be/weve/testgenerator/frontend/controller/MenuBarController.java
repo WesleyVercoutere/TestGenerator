674574c979ab86ca276e0a5eabf43853bcdb0578
Wesley Vercoutere <wesley.vercoutere@gmail.com>
2019-10-10 21:36:53 +0200

    update

--- src/main/java/be/weve/testgenerator/frontend/controller/MenuBarController.java
+++ src/main/java/be/weve/testgenerator/frontend/controller/MenuBarController.java
@@ -3,8 +3,8 @@ package be.weve.testgenerator.frontend.controller;
 import be.weve.testgenerator.frontend.RootLayout;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import org.springframework.stereotype.Component;
@@ -21,7 +21,7 @@ public class MenuBarController {
     HBox menuPane;
 
     @FXML
-    Image btnMaximize;
+    ImageView btnMaximize;
 
     //Constructor for DI from Spring
     public MenuBarController(RootLayout rootLayout) {
@@ -49,7 +49,20 @@ public class MenuBarController {
             }
         });
 
+        Image imageLarge = new Image("icon/window/maximize_dark.png");
+        Image imageRestore = new Image("icon/window/restore_dark.png");
 
+        Image image;
+
+
+
+        if (root.getStage().isMaximized()) {
+            image = imageRestore;
+        } else {
+            image = imageLarge;
+        }
+
+        btnMaximize.setImage(image);
 
     }
 
